7f43f04d8e69d47bbdd6d47b2ec6a3ce41a6f7f6
Yerassyl <erasik01@gmail.com>
2025-10-30 16:40:59 +0500

    added text, form expansion

--- src/sections/feedback/index.tsx
+++ src/sections/feedback/index.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import { Separator } from '../../components/separ'
 import './styles.scss'
 
 export function Feedback() {
+  const [expanded, setExpanded] = useState(false)
+
   return (
     <>
       <section className="section feedback">
@@ -12,36 +15,38 @@ export function Feedback() {
               <div className="h4">Напишите нам</div>
               <p>И мы ответим на любые интересующие вас вопросы</p>
             </div>
-            <div className="feedback__more">
-              <div className="feedback__group">
-                <input
-                  type=""
-                  placeholder="Имя *"
-                  className="feedback__input input"
-                />
+            {expanded && (
+              <div className="feedback__more">
+                <div className="feedback__group">
+                  <input
+                    type="text"
+                    placeholder="Имя *"
+                    className="feedback__input input"
+                  />
+                </div>
+                <div className="feedback__group">
+                  <input
+                    type="text"
+                    placeholder="Город, страна *"
+                    className="feedback__input input"
+                  />
+                </div>
+                <div className="feedback__group">
+                  <input
+                    type="tel"
+                    placeholder="Телефон *"
+                    className="feedback__input input"
+                  />
+                </div>
+                <div className="feedback__group">
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    className="feedback__input input"
+                  />
+                </div>
               </div>
-              <div className="feedback__group">
-                <input
-                  type=""
-                  placeholder="Город, страна *"
-                  className="feedback__input input"
-                />
-              </div>
-              <div className="feedback__group">
-                <input
-                  type=""
-                  placeholder="Телефон *"
-                  className="feedback__input input"
-                />
-              </div>
-              <div className="feedback__group">
-                <input
-                  type=""
-                  placeholder="Email"
-                  className="feedback__input input"
-                />
-              </div>
-            </div>
+            )}
           </div>
           <div className="section__body-col">
             <div className="feedback__group feedback__more-area">
@@ -50,13 +55,17 @@ export function Feedback() {
                 id=""
                 placeholder="Что вас интересует? *"
                 className="feedback__textarea textarea"
+                onFocus={() => setExpanded(true)}
+                onClick={() => setExpanded(true)}
               ></textarea>
             </div>
-            <div className="feedback__more">
-              <button type="submit" className="feedback__button btn">
-                Отправить
-              </button>
-            </div>
+            {expanded && (
+              <div className="feedback__more">
+                <button type="submit" className="feedback__button btn">
+                  Отправить
+                </button>
+              </div>
+            )}
           </div>
         </form>
         <Separator size="small" />
