6621c1eeb440c7c6a8a2dcb762c811f0d21afa86
Yerassyl <erasik01@gmail.com>
2025-11-19 16:34:41 +0500

    project page changes

--- src/pages/projects/project-page/index.tsx
+++ src/pages/projects/project-page/index.tsx
@@ -32,8 +32,82 @@ export function ProjectPage() {
         <Separator size="small" border="top" />
         <div className="section__body">
           <div className="section__body-col project-info">
-            <div className="project__header"></div>
-            <div className="section__content project__desc"></div>
+            <div className="project__header">
+              <div
+                className="section__header project__header-title"
+                data-scroll="title"
+              >
+                <div className="section__header-title">
+                  <h1>{project.title}</h1>
+                </div>
+                <div className="section__header-subtitle">Проектирование</div>
+                <div className="crumbs">
+                  <ul
+                    className="crumbs__list"
+                    itemScope
+                    itemType="https://schema.org/BreadcrumbList"
+                  >
+                    <li
+                      className="crumbs__list-point"
+                      itemProp="itemListElement"
+                      itemScope
+                      itemType="http://schema.org/ListItem"
+                    >
+                      <Link className="crumbs__item" to="/">
+                        <span itemProp="name">Главная</span>
+                      </Link>
+                      <meta itemProp="position" content="1" />
+                    </li>
+                    <li
+                      className="crumbs__list-point"
+                      itemProp="itemListElement"
+                      itemScope
+                      itemType="http://schema.org/ListItem"
+                    >
+                      <Link className="crumbs__item" to="/projects/">
+                        <span itemProp="name">Проекты</span>
+                      </Link>
+                      <meta itemProp="position" content="2" />
+                    </li>
+                    <li
+                      className="crumbs__list-point active"
+                      itemProp="itemListElement"
+                      itemScope
+                      itemType="http://schema.org/ListItem"
+                    >
+                      <Link
+                        className="crumbs__item"
+                        to={`/projects/${project.slug}`}
+                      >
+                        <span itemProp="name">{project.title}</span>
+                      </Link>
+                      <meta itemProp="position" content="3" />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="section__content project__header-list">
+                <ul>
+                  <li>
+                    <strong>Адрес: </strong>
+                    {project.address}
+                  </li>
+                  <li>
+                    <strong>Сроки строительства: </strong>
+                    {project.period}
+                  </li>
+                  <li>
+                    <strong>Нами проведены/проводятся: </strong>
+                    {project.services.join(', ')}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="section__content project__desc">
+              <h4>Описание проекта</h4>
+              <p>{project.shortDescription}</p>
+              <p>{project.extraDescription}</p>
+            </div>
             <div className="project__plan"></div>
             <div className="project__plan"></div>
           </div>
@@ -43,6 +117,8 @@ export function ProjectPage() {
             </ul>
           </div>
         </div>
+        <Separator size="small" border="bottom" />
+        <Separator />
       </section>
     </>
   )
